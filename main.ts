ca672149efbba9f0ed42b5fc9f6984ae79395ef0
1711095 <104539662+1711095@users.noreply.github.com>
2022-06-09 12:11:51 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,16 @@
+basic.forever(function () {
+    serial.writeLine("" + (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )))
+    if (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ) > 0) {
+        basic.showString("A")
+    } else {
+        basic.showString("B")
+    }
+})
